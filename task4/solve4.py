075b2bf43f37c1b7fc613cd5307cdf41763e8d03
itease <stepanov.ion@icloud.com>
2022-03-27 01:21:39 +0300

    Убрал повторения и добавил массив гласных в глобальную видимость

--- task4/solve4.py
+++ task4/solve4.py
@@ -1,11 +1,12 @@
+global vowels_all
+vowels_all = ['a', 'e', 'i', 'o', 'u', 'y'] + ['A', 'E', 'I', 'O', 'U', 'Y']
+
+
 def longermessage(message1, message2):
     return max(message1, message2)
 
 
 def vowelsscontains(message1, message2):
-    vowels_lower = ['a', 'e', 'i', 'o', 'u', 'y']
-    vowels_upper = ['A', 'E', 'I', 'O', 'U', 'Y']
-    vowels_all = vowels_lower + vowels_upper
     vowels1 = 0
     vowels2 = 0
     for letter in message1:
@@ -15,13 +16,9 @@ def vowelsscontains(message1, message2):
         if letter in vowels_all:
             vowels2 += 1
     return message1 if vowels1 > vowels2 else message2
-    # return message1 if message1.count(vowels_all) > message2.count(vowels_all) else message2
 
 
 def consonantscontains(message1):
-    vowels_lower = ['a', 'e', 'i', 'o', 'u', 'y']
-    vowels_upper = ['A', 'E', 'I', 'O', 'U', 'Y']
-    vowels_all = vowels_lower + vowels_upper
     vowels = 0
     for letter in message1:
         if letter in vowels_all:
@@ -44,7 +41,7 @@ def findwords(text1, wordtarget):
                 word += text1[j]
             else:
                 if wordtarget == word:
-                    complete = 'complete matches: ' + 'from ' + str(start) + ' to ' + str(j - 1)
+                    complete = 'complete matches: from ' + str(start) + ' to ' + str(j - 1)
                 elif wordtarget in word:
                     if text1 in [start, len(wordtarget) + start - 1] == wordtarget:
                         begin = str(start)
@@ -52,7 +49,7 @@ def findwords(text1, wordtarget):
                     else:
                         begin = str(j - len(wordtarget))
                         finish = str(j - 1)
-                    incomplete = 'incomplete matches: ' + 'from ' + begin + ' to ' + finish
+                    incomplete = 'incomplete matches: from ' + begin + ' to ' + finish
                 start = j + 1
                 break
     answer = complete + '\n' + incomplete
